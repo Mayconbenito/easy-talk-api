c5ae666321ed5a614f59f3384e240d5a05afb9cf
Mayconbenito <mayki17321@gmail.com>
2019-01-14 12:33:20 -0200

    jwt sign and verify created

---
+++ src/utils/jwt.js
@@ -0,0 +1,28 @@
+const jwt = require("jsonwebtoken");
+
+const sign = (body, cert) =>
+  new Promise((resolve, reject) => {
+    jwt.sign(body, cert, { algorithm: "HS256" }, (err, token) => {
+      if (err) {
+        reject(err);
+      }
+
+      resolve(token);
+    });
+  });
+
+const verify = (token, cert) =>
+  new Promise((resolve, reject) => {
+    jwt.verify(token, cert, (err, decoded) => {
+      if (err) {
+        reject(err);
+      }
+
+      resolve(decoded);
+    });
+  });
+
+module.exports = {
+  sign: sign,
+  verify: verify
+};
