35dabbe8aa2c0dcdbf7b347a901f75353e76f995
Mayconbenito <mayki17321@gmail.com>
2019-01-14 11:17:19 -0200

    body parser added

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 require("dotenv").config();
 
+app.use(express.json());
+
 const main = async () => {
   const mysql = require("mysql2/promise");
   const connection = await mysql.createConnection({
