e4b5ba0af92716bf24fdd5cf0effe6d6bc24be86
Mayconbenito <mayki17321@gmail.com>
2019-01-26 16:10:55 -0200

    peoples route added

--- src/authenticatedRouter.js
+++ src/authenticatedRouter.js
@@ -3,5 +3,36 @@ const router = express.Router();
 require("dotenv").config();
 
 module.exports = mysql => {
+  router.get("/peoples/:searchText/:page", async (req, res) => {
+    try {
+      const { searchText, page } = req.params;
+      const numberItems = 10;
+      const limit = numberItems * page - numberItems;
+
+      const [users] = await mysql.query(
+        `SELECT id, username, picture FROM users WHERE email = ? OR username LIKE ? LIMIT ${limit},${numberItems}`,
+        [searchText, `%${searchText}%`]
+      );
+
+      const [totalItems] = await mysql.query("SELECT FOUND_ROWS() as count");
+
+      if (users.length > 0) {
+        res.json({
+          metadata: {
+            totalItems: totalItems[0].count,
+            items: users.length,
+            pages: Math.ceil(page / totalItems[0].count)
+          },
+          users: users
+        });
+      } else {
+        res.status(200).json([]);
+      }
+    } catch (e) {
+      console.log(e);
+      res.status(500).json({ code: "INTERNAL_SERVER_ERROR" });
+    }
+  });
+
   return router;
 };
