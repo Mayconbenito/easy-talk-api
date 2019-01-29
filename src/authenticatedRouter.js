29f562940cd2aaeabb0ff3cb38672d1771e6fbe5
Mayconbenito <mayki17321@gmail.com>
2019-01-29 10:11:44 -0200

    contacts route with GET method added

--- src/authenticatedRouter.js
+++ src/authenticatedRouter.js
@@ -38,5 +38,36 @@ module.exports = mysql => {
     }
   });
 
+  router.get("/contacts/:page", async (req, res) => {
+    try {
+      const { page } = req.params;
+      const numberItems = 10;
+      const limit = numberItems * page - numberItems;
+
+      const [users] = await mysql.query(
+        `SELECT SQL_CALC_FOUND_ROWS users.id, users.username, users.picture FROM friends INNER JOIN users ON friends.user_b = users.id WHERE user_a = ? LIMIT ${limit},${numberItems}`,
+        [req.userId]
+      );
+
+      const [totalItems] = await mysql.query("SELECT FOUND_ROWS() as count");
+
+      if (users.length > 0) {
+        res.json({
+          metadata: {
+            totalItems: totalItems[0].count,
+            items: users.length,
+            pages: Math.ceil(totalItems[0].count / numberItems)
+          },
+          users: users
+        });
+      } else {
+        res.status(200).json([]);
+      }
+    } catch (e) {
+      console.log(e);
+      res.status(500).json({ code: "INTERNAL_SERVER_ERROR" });
+    }
+  });
+
   return router;
 };
