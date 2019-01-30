dd69a46fc739afb76865924f0e8fe0276e0bbcee
Mayconbenito <mayki17321@gmail.com>
2019-01-30 12:03:32 -0200

    socket.io session connection and disconnection handler created

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const server = require("http").Server(app);
 const io = require("socket.io")(server);
+const jwt = require("./utils/jwt");
 require("dotenv").config();
 
 app.use(express.json());
@@ -18,11 +19,31 @@ const main = async () => {
   const router = require("./router")(connection);
   app.use("/", router);
 
+  io.on("connection", async socket => {
+    const jwtKey = socket.handshake.query.jwt;
+
+    const jwtDecoded = await jwt.verify(jwtKey, process.env.JWT_HASH);
+    await connection.query(
+      "INSERT INTO sessions (device, status, websocket_id, started_at, user_id) VALUES (?,?,?,?,?)",
+      ["BROWSER", 1, socket.id, new Date(), jwtDecoded.id]
+    );
+
+    socket.on("disconnect", async () => {
+      console.log("Got disconnect!");
+
+      await connection.query(
+        "UPDATE sessions SET status = '0' WHERE websocket_id = ?",
+        [socket.id]
+      );
+    });
+  });
+
   const authenticatedRouter = require("./authenticatedRouter")(connection, io);
   app.use("/app", authenticatedRouter);
 };
 
+main();
+
 server.listen(process.env.SERVER_PORT, () => {
   console.log("Server running");
-  main();
 });
