feb237690ac821cfe18b50fc1894397c5f43c609
Mayconbenito <mayki17321@gmail.com>
2019-02-01 10:47:07 -0200

    validation schema created

---
+++ src/middlewares/validations.js
@@ -0,0 +1,43 @@
+const { check, validationResult } = require("express-validator/check");
+
+const validationSchema = {
+  login: [
+    check("email", "invalid email address").isEmail(),
+    check(
+      "password",
+      "password must contain between 6 and 60 characters"
+    ).isLength({ min: 6, max: 60 })
+  ],
+  register: [
+    check(
+      "username",
+      "username most contain between 1 and 10 characters"
+    ).isLength({ min: 1, max: 10 }),
+    check("email", "invalid email address").isEmail(),
+    check(
+      "password",
+      "password must contain between 6 and 60 characters"
+    ).isLength({
+      min: 6,
+      max: 60
+    })
+  ],
+  messages: [
+    {
+      post: [check("toId").isInt(), check("message").isString()]
+    }
+  ],
+  peoples: [
+    {
+      get: [check("page").isInt(), check("searchText").isString()]
+    }
+  ],
+  contacts: [
+    {
+      get: [check("page").isInt()],
+      post: [check("id").isInt()]
+    }
+  ]
+};
+
+module.exports = { validationSchema, validationResult };
