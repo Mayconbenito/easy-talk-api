50d63e03f2562de510ed87b6e4789e14600f59f7
Mayconbenito <mayki17321@gmail.com>
2019-02-06 08:49:59 -0200

    validation added to messages route

--- src/routes/restrict/messages.js
+++ src/routes/restrict/messages.js
@@ -1,44 +1,58 @@
 const router = require("express").Router();
 
+const {
+  validationSchema,
+  validationResult
+} = require("../../middlewares/validations");
+
 module.exports = (mysql, io) => {
-  router.post("/messages/:toId", async (req, res) => {
-    try {
-      const { message } = req.body;
-      const { toId } = req.params;
+  router.post(
+    "/messages/:toId",
+    validationSchema.messages.post,
+    async (req, res) => {
+      try {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+          return res.status(400).json({ errors: errors.array() });
+        }
 
-      const [session] = await mysql.query(
-        "SELECT * FROM sessions WHERE user_id = ? ORDER BY id DESC LIMIT 1",
-        [toId]
-      );
+        const { message } = req.body;
+        const { toId } = req.params;
 
-      if (session.length > 0 && session[0].status === 1) {
-        const [fromUser] = await mysql.query(
-          "SELECT * FROM users WHERE id = ?",
-          [req.userId]
+        const [session] = await mysql.query(
+          "SELECT * FROM sessions WHERE user_id = ? ORDER BY id DESC LIMIT 1",
+          [toId]
         );
 
-        io.to(session[0].socket_id).emit("message", {
-          message: message,
-          date_time: new Date(),
-          from: {
-            id: fromUser[0].id,
-            username: fromUser[0].username,
-            picture: fromUser[0].picture
-          }
-        });
+        if (session.length > 0 && session[0].status === 1) {
+          const [fromUser] = await mysql.query(
+            "SELECT * FROM users WHERE id = ?",
+            [req.userId]
+          );
+
+          io.to(session[0].socket_id).emit("message", {
+            message: message,
+            date_time: new Date(),
+            from: {
+              id: fromUser[0].id,
+              username: fromUser[0].username,
+              picture: fromUser[0].picture
+            }
+          });
+        }
+
+        await mysql.query(
+          "INSERT INTO messages (data, from_id, to_id, date_time) VALUES (?,?,?,?)",
+          [message, req.userId, toId, new Date()]
+        );
+
+        res.json({ code: "MESSAGE_SENT" });
+      } catch (e) {
+        console.log(e);
+        res.status(500).json({ code: "INTERNAL_SERVER_ERROR" });
       }
-
-      await mysql.query(
-        "INSERT INTO messages (data, from_id, to_id, date_time) VALUES (?,?,?,?)",
-        [message, req.userId, toId, new Date()]
-      );
-
-      res.json({ code: "MESSAGE_SENT" });
-    } catch (e) {
-      console.log(e);
-      res.status(500).json({ code: "INTERNAL_SERVER_ERROR" });
     }
-  });
+  );
 
   return router;
 };
