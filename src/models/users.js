598f7acf7b3bd4287d5b2c26079fcd2e62f237d9
Mayconbenito <mayki17321@gmail.com>
2019-03-08 11:04:58 -0300

    password select set to false

--- src/models/users.js
+++ src/models/users.js
@@ -3,7 +3,7 @@ const mongoose = require("../mongodb");
 const schema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
+  password: { type: String, required: true, select: false },
   createdAt: { type: Date, default: Date.now },
   contacts: { type: Array, required: true, ref: "Users" }
 });
