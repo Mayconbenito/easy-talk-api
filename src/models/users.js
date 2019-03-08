ac5f192339194cd5af5dbd1bba686f2a760b62e4
Mayconbenito <mayki17321@gmail.com>
2019-03-08 11:03:51 -0300

    contacts array added to Users model

--- src/models/users.js
+++ src/models/users.js
@@ -4,7 +4,8 @@ const schema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  createdAt: { type: Date, default: Date.now }
+  createdAt: { type: Date, default: Date.now },
+  contacts: { type: Array, required: true, ref: "Users" }
 });
 
 module.exports = mongoose.model("Users", schema);
