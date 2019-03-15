96320925e468f428c9edd309ce31102ba1d8200c
Mayconbenito <mayki17321@gmail.com>
2019-03-15 10:13:27 -0300

    session schema created on users model

--- src/models/users.js
+++ src/models/users.js
@@ -1,11 +1,18 @@
 const mongoose = require("../mongodb");
 
+const sessionSchema = new mongoose.Schema({
+  socketId: { type: String, required: true },
+  status: { type: String, enum: ["online", "offline"], required: true },
+  createdAt: { type: Date, default: Date.now }
+});
+
 const schema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true, select: false },
   createdAt: { type: Date, default: Date.now },
-  contacts: { type: Array, required: true, ref: "Users" }
+  contacts: { type: Array, required: true, ref: "Users" },
+  session: { sessionSchema }
 });
 
 module.exports = mongoose.model("Users", schema);
