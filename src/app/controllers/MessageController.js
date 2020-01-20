87b5476c4dd587601c7e88452b4e5d43dab79141
Maycon Benito <mayki17321@gmail.com>
2020-01-20 10:04:15 -0300

    chore: send message based on chatId

--- src/app/controllers/MessageController.js
+++ src/app/controllers/MessageController.js
@@ -6,22 +6,29 @@ export default {
   store: async (req, res) => {
     try {
       const { message } = req.body;
-      const { reciverId } = req.params;
+      const { chatId } = req.params;
 
-      const reciver = await Users.findById(reciverId);
+      const chat = await Chats.findOne({
+        _id: chatId
+      }).select("-messages");
+
+      if (!chat) {
+        return res.status(404).json({
+          code: "CHAT_NOT_EXISTS"
+        });
+      }
+
+      const sender = await Users.findById(req.user.id).select("-contacts");
+
+      const [reciverId] = chat.participants.filter(
+        participant => participant.toJSON() !== req.user.id
+      );
+
+      const reciver = await Users.findById(reciverId).select("-contacts");
 
       if (!reciver) {
         return res.status(404).json({ code: "USER_NOT_FOUND" });
       }
-
-      const senderUser = await Users.findById(req.user.id).select("-contacts");
-
-      const chat = await Chats.findOne({
-        $or: [
-          { participants: [reciverId, req.user.id] },
-          { participants: [req.user.id, reciverId] }
-        ]
-      }).select("_id");
 
       const messageObj = {
         _id: mongoose.Types.ObjectId(),
@@ -32,50 +39,24 @@ export default {
         type: "text"
       };
 
-      if (!chat) {
-        // Create chat and add the message
-        const chat = await Chats.create({
-          participants: [reciverId, req.user.id],
-          lastSentMessage: message,
-          messages: messageObj
-        });
+      await Chats.updateOne(
+        { _id: chat._id },
+        { lastSentMessage: message, $push: { messages: messageObj } }
+      ).select("-messages");
 
-        if (reciver.session && reciver.session.status === "online") {
-          req.io.to(reciver.session.socketId).emit("message", {
-            chat: { participants: chat.participants, _id: chat._id },
-            message,
-            sender: senderUser,
-            timestamp: new Date()
-          });
-        }
-
-        chat.messages = undefined;
-
-        return res.json({
-          chat: { ...chat.toJSON(), sender: senderUser },
-          message: messageObj
-        });
-      } else {
-        // Add the message to the chat if the chat already exists
-        const updatedChat = await Chats.findOneAndUpdate(
-          { _id: chat._id },
-          { lastSentMessage: message, $push: { messages: messageObj } }
-        ).select("-messages");
-
-        if (reciver.session && reciver.session.status === "online") {
-          req.io.to(reciver.session.socketId).emit("message", {
-            chat: updatedChat,
-            message,
-            sender: senderUser,
-            timestamp: new Date()
-          });
-        }
-
-        return res.json({
-          chat: { ...updatedChat.toJSON(), sender: senderUser },
-          message: messageObj
+      if (reciver.session && reciver.session.status === "online") {
+        req.io.to(reciver.session.socketId).emit("message", {
+          chat: { ...chat.toJSON(), lastSentMessage: messageObj.data, sender },
+          message,
+          sender,
+          timestamp: new Date()
         });
       }
+
+      return res.json({
+        chat: { ...chat.toJSON(), lastSentMessage: messageObj.data, sender },
+        message: messageObj
+      });
     } catch (e) {
       console.log(e);
       res.status(500).json({ code: "INTERNAL_SERVER_ERROR" });
