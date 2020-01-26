26559568f553a724dbf2f70be706dbe92ea1c777
Maycon Benito <mayki17321@gmail.com>
2020-01-26 17:59:13 -0300

    chore: change send message route

--- src/app/validators/ChatMessage.js
+++ src/app/validators/ChatMessage.js
@@ -4,6 +4,14 @@ import joiObjectId from "joi-objectid";
 Joi.objectId = joiObjectId(Joi);
 
 export default {
+  store: {
+    body: Joi.object().keys({
+      message: Joi.string().required()
+    }),
+    params: Joi.object().keys({
+      chatId: Joi.objectId().required()
+    })
+  },
   index: {
     params: Joi.object().keys({
       chatId: Joi.objectId().required()
