3aa53d689d8beafb1d5b228b110adb1e8fef4302
Maycon Benito <mayki17321@gmail.com>
2020-01-29 14:20:25 -0300

    chore: store messages on messages collection

--- src/app/models/chats.js
+++ src/app/models/chats.js
@@ -1,26 +1,12 @@
 import mongoose from "../../config/mongodb";
 
-const messagesSchema = new mongoose.Schema({
-  _id: { type: mongoose.Schema.ObjectId },
-  senderId: { type: mongoose.Schema.ObjectId, required: true, ref: "Users" },
-  reciverId: { type: mongoose.Schema.ObjectId, required: true, ref: "Users" },
-  data: { type: String, required: true },
-  status: { type: String, enum: ["sent", "recived", "read"], required: true },
-  type: {
-    type: String,
-    enum: ["text", "image", "audio", "video"],
-    required: true
-  },
-  createdAt: { type: Date, default: Date.now }
-});
-
 const schema = new mongoose.Schema({
   participants: [
     { type: mongoose.Schema.Types.ObjectId, required: true, ref: "Users" }
   ],
   messagesCount: { type: Number, default: 0 },
   lastSentMessage: { type: String },
-  messages: [messagesSchema],
+  messages: [{ type: mongoose.Schema.Types.ObjectId, ref: "Messages" }],
   createdAt: { type: Date, default: Date.now }
 });
 
