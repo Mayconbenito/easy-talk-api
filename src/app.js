fb3fa94fe6fb6c3e5af7aafd7a8d4f549cebc411
Maycon Benito <mayki17321@gmail.com>
2020-06-14 20:04:33 -0300

    chore: add helmet and gzip compression

--- src/app.js
+++ src/app.js
@@ -2,11 +2,13 @@ import express from "express";
 import http from "http";
 import socketIo from "socket.io";
 import { errors } from "celebrate";
+import helmet from "helmet";
+import compression from "compression";
 import * as Sentry from "@sentry/node";
 
 import "dotenv/config";
 import ioMiddleware from "./app/middlewares/io";
-import errorHandler from "./app/middlewares/errorHandler"
+import errorHandler from "./app/middlewares/errorHandler";
 import routes from "./routes";
 import { setupWS } from "./utils/websocket";
 
@@ -14,6 +16,8 @@ const app = express();
 
 Sentry.init({ dsn: process.env.SENTRY_DSN });
 
+app.use(helmet());
+app.use(compression());
 app.use(Sentry.Handlers.requestHandler());
 
 const server = http.Server(app);
@@ -28,6 +32,6 @@ setupWS(socket);
 
 app.use(Sentry.Handlers.errorHandler());
 
-app.use(errorHandler)
+app.use(errorHandler);
 
 export { server, app };
